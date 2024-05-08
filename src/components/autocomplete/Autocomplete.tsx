ebe6313618e36739738641df11ee02b01f66964a
vanardelli <stefano.valentini9@gmail.com>
2024-05-08 13:24:01 +0200

    added default text and html button

--- src/components/autocomplete/Autocomplete.tsx
+++ src/components/autocomplete/Autocomplete.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import getData from "../../api/getData";
 import { Game } from "./Autocomplete.types";
 import loadingGif from "../../assets/loading.gif";
@@ -7,12 +7,12 @@ function Autocomplete() {
   const [inputValue, setInputValue] = useState("");
   const [suggestionsList, setSuggestionsList] = useState<Game[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [selectedValue, setSelectedValue] = useState<string | undefined>(
-    undefined
-  );
+  const [isSearching, setIsSearching] = useState<string | undefined>(undefined);
+  const selectValue = useRef<string | undefined>(undefined);
 
   useEffect(() => {
-    if (inputValue !== selectedValue) {
+    if (inputValue !== selectValue.current) {
+      setIsSearching(undefined);
       setIsLoading(true);
       const timeoutId = setTimeout(() => {
         fetchSuggestions(inputValue);
@@ -65,27 +65,38 @@ function Autocomplete() {
         value={inputValue}
         onChange={(e) => handleInputChange(e)}
       ></input>
-      {isLoading && <img className="loading" src={loadingGif}></img>}
+      {isLoading && (
+        <img alt="loading" className="loading" src={loadingGif}></img>
+      )}
       {suggestionsList.length > 0 && (
         <div className="list-container">
           {suggestionsList.map((g) => (
-            <div
+            <button
               className="list-element"
               key={g.gameID}
               id={g.gameID}
               onClick={() => {
                 setInputValue(g.external);
-                setSelectedValue(g.external);
+                setIsSearching(g.external);
+                selectValue.current = g.external;
                 setSuggestionsList([]);
               }}
             >
               {highlightMatch(g.external)}
-            </div>
+            </button>
           ))}
         </div>
       )}
+
+      {!isLoading &&
+        suggestionsList.length === 0 &&
+        inputValue &&
+        !isSearching && (
+          <div className="list-container-empty">No value found.</div>
+        )}
+
       <div className="autocomplete-selected-value">
-        Selected value: {selectedValue}
+        Selected value: {selectValue.current}
       </div>
     </div>
   );
