b8c20f2247af95f88d9d441b0dae73a39a5d2236
vanardelli <stefano.valentini9@gmail.com>
2024-05-21 12:35:28 +0200

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            logLevel: "info",
+            logInfoToStdOut: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
